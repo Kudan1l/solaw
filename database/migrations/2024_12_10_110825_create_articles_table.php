5c2a2bf4bb28ed2fb393adb77132cad31d0ac1ad
Kudan1l <alpsosial@gmail.com>
2024-12-10 18:13:44 +0700

    Menambahkan project Laravel

---
+++ database/migrations/2024_12_10_110825_create_articles_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('author');
+            $table->longText('description');
+            $table->longText('content');
+            $table->string('thumbnail_url');
+            $table->timestamps();
+        });
+
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+        });
+
+        Schema::create('article_category', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('article_id')->constrained('articles')->onDelete('cascade');
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+            $table->timestamps();
+            $table->unique(['article_id', 'category_id']); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+        Schema::dropIfExists('categories');
+        Schema::dropIfExists('article_category');
+    }
+};
