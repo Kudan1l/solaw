45d7b4c72cf6ba16d4b9bb73892b132ecadcb196
Kudan1l <alpsosial@gmail.com>
2024-12-11 23:09:08 +0700

    Menambahkan Navigation Menu dan Footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,25 @@
+<footer>
+    <div class="container">
+        <div class="footer-content d-flex gap-5 mb-3">
+            <div class="about w-100">
+            <h5>Tentang Kami</h5>
+                <p>Solution of Law (SOLAW) adalah platform inovatif yang bertujuan untuk mempermudah akses masyarakat terhadap layanan hukum. Kami memahami bahwa proses hukum sering kali rumit dan memakan waktu, sehingga kami hadir untuk memberikan solusi yang efisien dan transparan.</p>
+            </div>
+            <div class="contac w-50">
+            <h5>Hubungi Kami</h5>
+            <p><i class="bi bi-geo-alt-fill"></i>Telkom Univeristy</p>
+            <p><i class="bi bi-envelope-fill"></i>solaw@gmail.com</p>
+            <p><i class="bi bi-telephone-fill"></i>+62 1234-5678-912</p>
+
+            <div class="social-media">
+                <a href="https://www.instagram.com"><i class="bi bi-instagram"></i></a>
+                <a href="https://www.youtube.com"><i class="bi bi-youtube"></i></a>
+                <a href="https://github.com/Kudan1l/solaw.git"><i class="bi bi-github"></i></a>
+            </div>
+          </div>
+        </div>
+        <div class="text-center py-2 copyright">
+          <small>&copy; 2024 Kelompok 10 IT-46-02.</small>
+        </div>
+    </div>
+</footer>
